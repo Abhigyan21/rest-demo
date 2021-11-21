8ff3d5e41375db75b523be1a203725501b8a0053
Abhigyan21 <abhigyanmandal2@gmail.com>
2021-11-22 00:13:59 +0530

    Modified codebase

--- src/main/java/com/abhigyan/restdemo/RestDemoApplication.java
+++ src/main/java/com/abhigyan/restdemo/RestDemoApplication.java
@@ -15,7 +15,7 @@ public class RestDemoApplication {
 
 	String htmlReturnData = "<html>" +
 			"<head><title>K8S Azure</title></head>" +
-			"<body><p>Welcome to K8S on Azure via SpringBoot</p></body>" +
+			"<body><h1>Welcome to K8S on Azure via SpringBoot</h1></body>" +
 			"</html>";
 
 	@GetMapping("/home")
